7f5e38e05b01a246d4493dd9872d8fdf24fdcdc9
Bosco <andrijpeh489@gmail.com>
2024-02-24 11:55:34 +0200

    Додаємо функцію check_guess():

--- D.Z.py
+++ D.Z.py
@@ -13,19 +13,46 @@ def generate_number():
         min_val=int(min_val)
         max_val=int(max_val)
         secret_number=random.randint(min_val, max_val)
-        result.config(text="", fg="black")  # Set text color to black
+        result.config(text="", fg="black")
         secret_number_global = secret_number
         root.title("Вгадай число - Спроби: 0")
         min_entry.config(state="disabled")
         max_entry.config(state="disabled")
     else:
-        result.config(text="Введіть коректні значення!", fg="red")  # Set text color to red
-
+        result.config(text="Введіть коректні значення!", fg="red")
+def check_guess():
+    global attempts
+    global secret_number_global
+    if secret_number_global is None:
+        messagebox.showerror("Помилка", "Спочатку згенеруйте число!")
+        return
+    guess = vgadatu_entry.get()
+    if guess.isdigit():
+        guess = int(guess)
+        attempts += 1
+        root.title(f"Вгадай число - Спроби: {attempts}")
+        if guess < secret_number_global:
+            result.config(text="Загадане число більше", fg="blue",background='red',)
+        elif guess > secret_number_global:
+            result.config(text="Загадане число менше", fg="red",background='blue',)
+        else:
+            result.config(text="Вітаю! Ви вгадали число!", fg="green")
+            messagebox.showinfo("Вітаю!", f"Ви вгадали число за {attempts} спроб")
+            vgadatu_entry.config(state="disabled")
+    else:
+        result.config(text="Введіть коректне число!", fg="red")
+    if attempts == 5:
+        vgadatu_entry.config(state="disabled")
+    if attempts >= 5:
+        result.config(text="Ви використали всі спроби. Спробуйте ще раз.", fg="red")
+        answer = messagebox.askyesno("Вгадай число", "Бажаєте розпочати нову гру?")
+        if answer:
+            generate_number()
 root = Tk()
 root.title("Вгадай число - Спроби: 0")
 root.configure(bg='green')  # Set background color to light grey
-Label(root, text="Мінімальне значення:", fg="green",background='red',).grid(row=0, column=0)  # Set text color to black
-Label(root, text="Максимальне значення:", fg="black",background='blue',).grid(row=1, column=0)  # Set text color to black
+Label(root, text="Мінімальне значення:", fg="green",background='red',).grid(row=0, column=0)
+Label(root, text="Максимальне значення:", fg="black",background='blue',).grid(row=1, column=0)
 min_entry = Entry(root)
 max_entry = Entry(root)
 min_entry.grid(row=0, column=1)
